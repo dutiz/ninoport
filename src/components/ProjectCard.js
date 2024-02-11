c0483de2a9e8c7483b621e2b011ecae4ed539b8a
dutiz <zaimidaut@gmail.com>
2024-02-11 22:29:56 +0100

    project card update

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,33 +1,44 @@
-'use client'
+"use client";
 
+import SVG from "react-inlinesvg";
 import { PrismicText } from "@prismicio/react";
 import { PrismicNextLink } from "@prismicio/next";
+import { AnimationOnScroll } from "react-animation-on-scroll";
 
 export function ProjectCard({ project }) {
   return (
     <div className="lg:col-6 mb-8">
-      <PrismicNextLink
-        document={project}
-        href={`/projects/${project.uid}`}
-        tabIndex="-1"
+      <AnimationOnScroll
+        animateIn="animate__fadeIn"
+        animateOut="animate__fadeOut"
       >
-        <div
-          style={{
-            background: `url('${project.data.cover_image.url}') no-repeat center center / cover`,
-            height: "230px",
-          }}
-          className="relative group"
+        <PrismicNextLink
+          document={project}
+          href={`/projects/${project.uid}`}
+          tabIndex="-1"
         >
-          <div className="absolute inset-0 px-10 py-12 transition-all duration-500 ease-in-out bg-yellow-300 opacity-0 group-hover:opacity-70">
-            <h1 className="text-3xl font-semibold">
-              <PrismicText field={project.data.title} />
-            </h1>
-            <p className="text-3xl font-semibold">
-              <PrismicText field={project.data.description} />
-            </p>
+          <div
+            style={{
+              background: `url('${project.data.cover_image.url}') no-repeat center center / cover`,
+              height: "230px",
+            }}
+            className="relative group text-center"
+          >
+            <div className="absolute inset-0 px-10 py-12 transition-all duration-500 ease-in-out bg-yellow-300 bg-opacity-0 group-hover:bg-opacity-70">
+              <h1 className="opacity-0 group-hover:opacity-100 text-3xl font-semibold">
+                <PrismicText field={project.data.title} />
+              </h1>
+              <p className="mt-1 opacity-0 group-hover:opacity-100 text-sm">
+                <PrismicText field={project.data.description} />
+              </p>
+              <SVG
+                src="/svg/link-svg.svg"
+                className="mt-4 w-9 h-9 opacity-0 group-hover:opacity-100 fill-current mx-auto"
+              />
+            </div>
           </div>
-        </div>
-      </PrismicNextLink>
+        </PrismicNextLink>
+      </AnimationOnScroll>
     </div>
   );
 }
