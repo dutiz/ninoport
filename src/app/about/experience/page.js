515a58f37fb1ab2753471ca64268b7806f9fbadf
dutiz <zaimidaut@gmail.com>
2024-02-28 16:09:03 +0100

    added experience section with button functions and sliders

--- src/app/about/experience/page.js
+++ src/app/about/experience/page.js
@@ -1,6 +1,7 @@
 import { createClient } from "@/prismicio";
 import { Layout } from "@/components/Layout";
 import AboutSliderSection from "@/components/AboutSliderSection";
+import ExperienceSection from "@/components/ExperienceSection";
 
 export default async function Experience() {
   const client = createClient();
@@ -15,34 +16,8 @@ export default async function Experience() {
       <div className="bg-gray-300 dark:bg-gray-900">
         <div className="container">
           <div className="row justify-center">
-            <div className="col-10">
-              <div className="row">
-                <div className="lg:col-6 md:text-start text-center">
-                  <h1 className="mt-20 inline-block px-14 py-2 text-black dark:text-white text-2xl md:text-4xl border border-yellow-500 rounded-2xl">
-                    Lucky Media
-                  </h1>
-                </div>
-                <div className="lg:col-6 md:text-end text-center">
-                  <h1 className="my-20 minline-block px-14 py-2 text-black dark:text-white text-2xl md:text-4xl border border-yellow-500 rounded-2xl">
-                    Bridge Technologies
-                  </h1>
-                </div>
-              </div>
-              <div className="row justify-center mt-20">
-                <div className="col-10">
-                  <p className="md:text-4xl text-2xl text-black dark:text-white">
-                    LuckyMedia was one of the first internships that i made when i was studying.
-                    In this company i started to learn Front End with the technologies:
-                    HTML5, CSS3, Javascript.After learning these technologies i started to learn the frameworks 
-                    ReactJS, NextJS and also TailwindCSS.
-                  </p>
-                </div>
-              </div>
+            <ExperienceSection/>
             </div>
-            <div className="col-12 mt-32">
-                <AboutSliderSection />
-            </div>
-          </div>
         </div>
       </div>
     </Layout>
---
+++ src/components/ExperienceSection.js
@@ -0,0 +1,123 @@
+"use client";
+import "swiper/css";
+
+import React, { useState } from "react";
+import SVG from "react-inlinesvg";
+import { Autoplay } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+export default function ExperienceSection() {
+  const [isBridge, setIsBridge] = useState(false);
+  return (
+    <div className="col-10">
+      <div className="row">
+        <div className="lg:col-6 md:text-start text-center">
+          <button
+            onClick={() => setIsBridge(false)}
+            className="mt-20 inline-block px-14 py-2 text-black dark:text-white text-2xl md:text-4xl border border-yellow-500 rounded-2xl"
+          >
+            Lucky Media
+          </button>
+        </div>
+        <div className="lg:col-6 md:text-end text-center">
+          <button
+            onClick={() => setIsBridge(true)}
+            className="my-20 minline-block px-14 py-2 text-black dark:text-white text-2xl md:text-4xl border border-yellow-500 rounded-2xl"
+          >
+            Bridge Technologies
+          </button>
+        </div>
+      </div>
+      <div className="row justify-center mt-20">
+        <div className="col-10">
+          {isBridge ? (
+            <p className="md:text-4xl text-2xl text-black dark:text-white">
+              Bridge Technology was one of the first internships that i made
+              when i was studying. In this company i started to learn Front End
+              with the technologies: HTML5, CSS3, Javascript.After learning
+              these technologies i started to learn the frameworks ReactJS,
+              NextJS and also TailwindCSS.
+            </p>
+          ) : (
+            <p className="md:text-4xl text-2xl text-black dark:text-white">
+              LuckyMedia was one of the first internships that i made when i was
+              studying. In this company i started to learn Front End with the
+              technologies: HTML5, CSS3, Javascript.After learning these
+              technologies i started to learn the frameworks ReactJS, NextJS and
+              also TailwindCSS.
+            </p>
+          )}
+        </div>
+      </div>
+      <div className="row mt-32 justify-center">
+        <div className="col-12 py-20">
+          <Swiper
+            loop={true}
+            slidesPerView={1}
+            spaceBetween={10}
+            autoplay={{
+              delay: 1000,
+              disableOnInteraction: false,
+            }}
+            breakpoints={{
+              640: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+              },
+              768: {
+                slidesPerView: 4,
+                spaceBetween: 40,
+              },
+              1024: {
+                slidesPerView: 5,
+                spaceBetween: 50,
+              },
+            }}
+            modules={[Autoplay]}
+            className="mySwiper"
+          >
+            {isBridge ? (
+                // Bridge Slider
+              <>
+                <SwiperSlide>
+                  <SVG src="/svg/figma-svg.svg" className="mx-auto" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <SVG src="/svg/react-svg.svg" className="mx-auto" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <SVG src="/svg/ae-svg.svg" className="mx-auto" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <SVG src="/svg/js-svg.svg" className="mx-auto" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <SVG src="/svg/technology-1.svg" className="mx-auto" />
+                </SwiperSlide>
+              </>
+            ) : (
+                // Lucky Slider
+              <>
+                <SwiperSlide>
+                  <SVG src="/svg/figma-svg.svg" className="mx-auto" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <SVG src="/svg/react-svg.svg" className="mx-auto" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <SVG src="/svg/ae-svg.svg" className="mx-auto" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <SVG src="/svg/js-svg.svg" className="mx-auto" />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <SVG src="/svg/technology-1.svg" className="mx-auto" />
+                </SwiperSlide>
+              </>
+            )}
+          </Swiper>
+        </div>
+      </div>
+    </div>
+  );
+}
