8ab8c584594a3ab2bb2012f45189e9380fabed7c
dutiz <zaimidaut@gmail.com>
2024-03-08 22:47:05 +0100

    added favicons

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,34 @@ const poppins = Poppins({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${poppins.className} font-sans overflow-x-hidden antialiased`}>
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/favicon/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon/favicon-16x16.png"
+      />
+      <link rel="manifest" href="/favicon/site.webmanifest" />
+      <link
+        rel="mask-icon"
+        href="/favicon/safari-pinned-tab.svg"
+        color="#5bbad5"
+      />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="theme-color" content="#ffffff"></meta>
+      <body
+        className={`${poppins.className} font-sans overflow-x-hidden antialiased`}
+      >
         <main>
           <Providers>
             {children}
