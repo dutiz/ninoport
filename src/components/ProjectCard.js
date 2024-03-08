b88326faceeb9e3cb2f6fd862ef69e39e0aa0ae8
dutiz <zaimidaut@gmail.com>
2024-03-08 21:53:56 +0100

    added border radius to project card

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -22,15 +22,12 @@ export function ProjectCard({ project }) {
               background: `url('${project.data.cover_image.url}') no-repeat center center / cover`,
               height: "230px",
             }}
-            className="relative group text-center"
+            className="relative rounded-[10px] group text-center"
           >
-            <div className="absolute inset-0 px-10 py-12 transition-all duration-500 ease-in-out bg-yellow-300 bg-opacity-0 group-hover:bg-opacity-70">
-              <h1 className="opacity-0 group-hover:opacity-100 text-3xl font-semibold">
+            <div className="absolute inset-0 rounded-[10px] px-10 py-12 transition-all duration-500 ease-in-out bg-yellow-300 bg-opacity-0 group-hover:bg-opacity-70">
+              <h1 className="opacity-0 group-hover:opacity-100 my-5 text-3xl font-semibold">
                 <PrismicText field={project.data.title} />
               </h1>
-              <p className="mt-1 opacity-0 group-hover:opacity-100 text-sm">
-                <PrismicText field={project.data.description} />
-              </p>
               <SVG
                 src="/svg/link-svg.svg"
                 className="mt-4 w-9 h-9 opacity-0 group-hover:opacity-100 fill-current mx-auto"
